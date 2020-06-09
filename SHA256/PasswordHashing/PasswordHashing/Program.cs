52b47f8f62d6bdabe29dfa6edb69380278b95d0c
Koushik HM <koushik.hm@ivygdcad.ivy-support.com>
2020-06-09 14:25:18 +0530

    first commit

---
+++ SHA256/PasswordHashing/PasswordHashing/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordHashing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string salt = null;
+
+            string hash = null;
+
+            bool check;
+
+            CryptographyProcessor proc = new CryptographyProcessor();
+
+            //salt=proc.CreateSalt(32);
+
+            //hash=proc.GenerateHash("password", "XcQ0tql2NY0NXrQjP+dGHr64gB9VDvEHBS8uOwRwcQ0=");
+
+            check=proc.AreEqual("password", "uHKWLW6/XqUuJuIPlKakVa/RpdRcOYjUchFfQiOYx5M=", "XcQ0tql2NY0NXrQjP+dGHr64gB9VDvEHBS8uOwRwcQ0=");
+
+
+
+            Console.WriteLine(check);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
